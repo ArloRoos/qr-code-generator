182e28ab1a3ba4f18dd5c75d365d20a3a807e1df
aroos <aroos2147@gmail.com>
2026-04-11 12:58:46 -0700

    Initial project setup

---
+++ src/main/java/com/aroos/qr/generator/Main.java
@@ -0,0 +1,12 @@
+package com.aroos.qr.generator;
+
+/**
+ * The {@link Main} class implements
+ */
+public final class Main
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
