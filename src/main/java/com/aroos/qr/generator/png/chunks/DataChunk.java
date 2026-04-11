9f7039a6946c74d96a5b5562577622c6f3164b2c
aroos <aroos2147@gmail.com>
2026-04-11 15:28:19 -0700

    Implemented deflator

--- src/main/java/com/aroos/qr/generator/png/chunks/DataChunk.java
+++ src/main/java/com/aroos/qr/generator/png/chunks/DataChunk.java
@@ -1,14 +1,19 @@
 package com.aroos.qr.generator.png.chunks;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.zip.DeflaterOutputStream;
+
 /**
  * The {@link DataChunk} class implements a {@link PNGChunk} representing the
  * actual image data of the PNG.
  */
 public final class DataChunk extends PNGChunk
 {
-    private final byte[][] pixels;
+    private final int[][] pixels;
 
-    public DataChunk(final byte[][] pixels)
+    public DataChunk(final int[][] pixels)
     {
         super("IDAT");
 
@@ -21,6 +26,23 @@ public final class DataChunk extends PNGChunk
     @Override
     byte[] getData()
     {
-        return new byte[10];
+        try (
+            final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            final DeflaterOutputStream deflate = new DeflaterOutputStream(bytes))
+        {
+            for (int i = 0; i < pixels.length; i++)
+            {
+                for (int j = 0; j < pixels[i].length; j++)
+                {
+                    deflate.write(pixels[i][j]);
+                }
+            }
+
+            return bytes.toByteArray();
+        }
+        catch (final IOException error)
+        {
+            throw new UncheckedIOException(error);
+        }
     }
 }
